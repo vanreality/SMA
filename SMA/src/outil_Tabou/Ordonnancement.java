7a1afd8e2ac35ed186bfb69ad768970dc202f3db
Pierrick Gervasi <Pierrick Gervasi@gmail.com>
2019-04-30 09:25:23 +0200

    BaseDeDonneeIA

--- SMA/src/outil_Tabou/Ordonnancement.java
+++ SMA/src/outil_Tabou/Ordonnancement.java
@@ -511,7 +511,7 @@ public class Ordonnancement {
 		
 		 Donnees donnees=new Donnees();
 		 
-		 Voyage Voyage1=new Voyage(donnees,500);
+		 Voyage Voyage1=new Voyage(donnees,5);
 		 
 		 Ordonnancement ordo1= new Ordonnancement(Voyage1.villesOrdonneesParId, Voyage1);
 		 
